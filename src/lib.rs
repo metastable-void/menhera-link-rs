9b43eea14e4a156f7fe6907ed99b0d24fc7a2c74
metastable-void <git@yuka.h09.eu>
2024-03-10 23:57:06 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -106,9 +106,14 @@ pub fn resolve_socket_addr(ip_version: IpVersion, addr: &str) -> Result<SocketAd
   Ok(found_sockaddr)
 }
 
+#[non_exhaustive]
 pub enum Packet {
   SimpleEncryption {
     ciphertext: Vec<u8>,
+  },
+
+  NoEncryption {
+    data: Vec<u8>,
   },
 }
 
@@ -125,6 +130,11 @@ impl Packet {
         return Ok(Packet::SimpleEncryption { ciphertext: buf.to_vec() });
       }
 
+      1 => {
+        // NoEncryption
+        return Ok(Packet::NoEncryption { data: buf.to_vec() });
+      }
+
       _ => {
         return Err(Box::new(io::Error::new(io::ErrorKind::InvalidInput, "Unrecognized packet type")));
       }
@@ -138,6 +148,10 @@ impl Packet {
         let packet_type_arr = [0u8];
         encoded = [&packet_type_arr as &[u8], ciphertext.as_slice()].concat();
       }
+      Self::NoEncryption { data } => {
+        let packet_type_arr = [1u8];
+        encoded = [&packet_type_arr as &[u8], data.as_slice()].concat();
+      }
     }
     encoded
   }
@@ -147,7 +161,7 @@ pub struct Server {
   socket: Arc<UdpSocket>,
   tap: Tun,
   remote_addr_str: String,
-  shared_secret: Arc<Vec<u8>>,
+  shared_secret: Option<Arc<Vec<u8>>>,
   ip_version: IpVersion,
 }
 
@@ -164,10 +178,111 @@ impl Server {
     Ok(Server {
       socket,
       tap,
-      shared_secret: Arc::new(shared_secret.to_owned()),
+      shared_secret: Some(Arc::new(shared_secret.to_owned())),
       remote_addr_str: remote_addr.to_owned(),
       ip_version,
     })
+  }
+
+  async fn run_plain(self: Self) -> Result<(), Box<dyn stf::error::Error>> {
+    let Server {
+      socket,
+      tap,
+      shared_secret,
+      remote_addr_str,
+      ip_version,
+    } = self;
+
+    if let Some(_) = shared_secret {
+      return Err(Box::new(io::Error::new(io::ErrorKind::InvalidInput, "Shared secret not provided")));
+    }
+
+    let mut tap_buf = [0u8; 65536];
+    let mut socket_buf = [0u8; 65536];
+
+    let mut remote_addr_cache: Option<SocketAddr> = None;
+    let mut remote_addr_cached_time = Instant::now();
+
+    let socket_receive = socket.clone();
+    let socket_send = socket_receive.clone();
+    let (mut tap_reader, mut tap_writer) = tokio::io::split(tap);
+
+    loop {
+      tokio::select! {
+        Ok(nread) = tap_reader.read(&mut tap_buf) => {
+          let plaintext = tap_buf[.. nread];
+
+          if let Some(_remote_addr) = remote_addr_cache {
+            let elapsed = remote_addr_cached_time.elapsed();
+            if elapsed.as_secs() > 60 {
+              if let Ok(remote_addr) = resolve_socket_addr(ip_version, &remote_addr_str) {
+                remote_addr_cache = Some(remote_addr);
+                remote_addr_cached_time = Instant::now();
+              }
+            }
+          } else {
+            if let Ok(remote_addr) = resolve_socket_addr(ip_version, &remote_addr_str) {
+              remote_addr_cache = Some(remote_addr);
+              remote_addr_cached_time = Instant::now();
+            }
+          }
+
+          if let Some(remote_addr) = remote_addr_cache {
+            let packet = Packet::NoEncryption { data: plaintext.to_vec() };
+            let packet_data = packet.as_vec();
+            if let Ok(nsent) = socket_send.send_to(&packet_data, &remote_addr.clone()).await {
+              trace!("Sent {} bytes to {:?}", nsent, &remote_addr);
+            } else {
+              warn!("Failed to send data to {:?}", &remote_addr);
+            }
+          } else {
+            info!("Remote address not resolved (yet). Discarding packet...");
+          }
+        }
+
+        Ok((nread, peer)) = socket_receive.recv_from(&mut socket_buf) => {
+          if let Ok(packet) = Packet::new(&socket_buf[.. nread]) {
+            trace!("Received {} bytes from {:?}", nread, peer);
+
+            if let Some(_remote_addr) = remote_addr_cache {
+              let elapsed = remote_addr_cached_time.elapsed();
+              if elapsed.as_secs() > 60 {
+                if let Ok(remote_addr) = resolve_socket_addr(ip_version, &remote_addr_str) {
+                  remote_addr_cache = Some(remote_addr);
+                  remote_addr_cached_time = Instant::now();
+                }
+              }
+            } else {
+              if let Ok(remote_addr) = resolve_socket_addr(ip_version, &remote_addr_str) {
+                remote_addr_cache = Some(remote_addr);
+                remote_addr_cached_time = Instant::now();
+              }
+            }
+  
+            if let Some(remote_addr) = remote_addr_cache {
+              if peer != remote_addr {
+                info!("Received packet from unexpected address. Discarding...");
+              } else {
+                match packet {
+                  Packet::NoEncryption { data } => {
+                    if let Ok(()) = tap_writer.write_all(&data).await {
+                      trace!("{} bytes data written to tap interface", &data.len());
+                    } else {
+                      warn!("Failed to write data to tap interface");
+                    }
+                  }
+                  _ => {
+                    // NOOP
+                  }
+                }
+              }
+            } else {
+              info!("Remote address not resolved (yet). Discarding incoming packet...");
+            }
+          }
+        }
+      }
+    }
   }
 
   #[allow(unreachable_patterns)]
@@ -179,6 +294,19 @@ impl Server {
       remote_addr_str,
       ip_version,
     } = self;
+
+    if let None = shared_secret {
+      return Server::run_plain(Server {
+        socket,
+        tap,
+        shared_secret,
+        remote_addr_str,
+        ip_version,
+      }).await;
+    }
+
+    let shared_secret = shared_secret.unwrap();
+    
     let mut tap_buf = [0u8; 65536];
     let mut socket_buf = [0u8; 65536];
     info!("Server running...");
